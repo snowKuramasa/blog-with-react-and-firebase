66eab8a921d5e302bbea8c101ddc139f508d8bc7
kuramasa <kuramasa.green@gmail.com>
2023-07-19 09:01:23 +0900

    cssとアイコン追加

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Navbar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faFilePen, faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function Navbar() {
   return (
     <nav>
-     <Link to="/">ホーム</Link>
-     <Link to="/createpost">記事投稿</Link>
-     <Link to="/login">ログイン</Link>
+     <Link to="/">
+        <FontAwesomeIcon icon={faHouse} />
+        ホーム
+      </Link>
+      <Link to="/createpost">
+        <FontAwesomeIcon icon={faFilePen} />
+        記事投稿
+      </Link>
+      <Link to="/login">
+        <FontAwesomeIcon icon={faArrowRightFromBracket} />
+        ログイン
+      </Link>
     </nav>
   )
 }
