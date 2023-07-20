5c802896f7ec3b4f9737717334fe8d8948393f7f
kuramasa <kuramasa.green@gmail.com>
2023-07-20 22:47:17 +0900

    ログイン処理追加

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,29 @@
+import { signInWithPopup } from 'firebase/auth'
 import React from 'react'
+import {auth, provider} from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
-export default function Login() {
+
+export default function Login({setIsAuth}) {
+
+  //リダイレクト処理
+  const navigate = useNavigate();
+
+  const loginWithGoogle = () => {
+    //Googleでログイン
+    signInWithPopup(auth, provider).then((result) => {
+      //ローカルストレージへログイン情報を保存
+      localStorage.setItem("isAuth",true)
+      setIsAuth(true)
+      console.log(result)
+      navigate("/")
+    });
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginWithGoogle}>Googleでログイン</button>
+    </div>
   )
 }
