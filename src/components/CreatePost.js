d36b01199f6eb3f41f7d2b40271df3e7a3d0d333
kuramasa <kuramasa.green@gmail.com>
2023-07-30 21:27:51 +0900

    記事投稿用ボタン用意

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./CreatePost.css"
 
 export const CreatePost = () => {
+
+  const [title, setTitle] = useState();
+  const [postText, setPostText] = useState();
+  const createPost = () => {
+    console.log(title)
+    console.log(postText)
+  }
+
+
   return (
-    <div>CreatePost</div>
+    <div className='createPostPage'>
+      <div className='postContainer'>
+        <h1>記事を投稿する</h1>
+        <div className='inputPost'>
+          <div>タイトル</div>
+          <input
+            type='test'
+            placeholder='タイトルを記入'
+            onChange={(e) => setTitle(e.target.value)}/>
+        </div>
+        <div className='inputPost'>
+          <div>投稿</div>
+          <textarea 
+            placeholder='投稿内容を記入'
+            onChange={(e) => setPostText(e.target.value)}
+          />
+        </div>
+        <button className='postButton' onClick={createPost}>投稿する</button>
+      </div>
+    </div>
   )
 }
