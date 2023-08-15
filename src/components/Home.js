f072e8c69a0e7e6f30f30a502cdfa416090fd6c4
kuramasa <kuramasa.green@gmail.com>
2023-08-15 09:02:31 +0900

    homeからfirestoreのデータ取得設定

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Home.css';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from "../firebase"
 
 function Home() {
+
+  //マウント時に発火(第二引数に空配列渡すとマウント時になる)
+  useEffect (() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db,"posts"))
+      console.log(data)
+      console.log(data.docs)
+      console.log(data.docs.map((doc) => ({doc})))
+      //スプレッド構文で展開し、fireStoreのdata()で階層深くの値を取得
+      //別途idプロパティを追加し、スプレッド構文で展開した中のidをセット
+      console.log(data.docs.map((doc) => ({ ...doc.data(),id: doc.id})))
+    }
+    getPosts()
+  }, [])
+
+
   return (
     <div className='homePage'>
       <div className='postContents'>
